8ec4d22b11d7c41c59248bb0ff4c993cfe13ae1a
stealthio <20127926+stealthio@users.noreply.github.com>
2020-10-15 13:17:18 +0200

    Implemented #3

--- main.lua
+++ main.lua
@@ -5,12 +5,21 @@ function contains(list, x)
     return false
 end
 
+local blocks_dug_in_iteration = 0
+
 local function dig_neighbours_block(pos)
+	if blocks_dug_in_iteration >= woodchopper_maximum_blocks_dug then
+		return
+	end
 	local list_pos = minetest.find_nodes_in_area({x=pos.x+1, y=pos.y+1, z=pos.z+1}, {x=pos.x-1, y=pos.y-1, z=pos.z-1}, woodchopper_diggable_blocks)
 
 	for i=1, #list_pos do
 		if list_pos[i] then
+			if blocks_dug_in_iteration >= woodchopper_maximum_blocks_dug then
+				return
+			end
 			minetest.dig_node(list_pos[i])
+			blocks_dug_in_iteration = blocks_dug_in_iteration + 1
 			dig_neighbours_block(list_pos[i])
 		end
 	end
@@ -20,6 +29,7 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 	if contains(woodchopper_diggable_blocks, oldnode.name) then
 		if(digger:is_player()) then
 			if(digger:get_player_control()[woodchopper_activation_button]) then
+				blocks_dug_in_iteration = 0
 				dig_neighbours_block(pos)
 			end
 		end
