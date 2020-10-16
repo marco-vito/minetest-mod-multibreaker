c1f3a2b423a7c82184351eb83d9190d42e31e901
stealthio <20127926+stealthio@users.noreply.github.com>
2020-10-16 10:44:44 +0200

    Added mining functionality and reworked config structure

--- config.lua
+++ config.lua
@@ -1,7 +1,4 @@
---[[Configures all the types os blocks that are affect by the multi-dig. By
-default it aims towards wood, but it coul teoretically be used for other types
-of blocks.]]--
-woodchopper_diggable_blocks = {
+woodchopper_wood = {
 	"default:tree",
 	"default:jungletree",
 	"default:pine_tree",
@@ -15,15 +12,45 @@ woodchopper_diggable_blocks = {
 	"mcl_core:jungletree"
 }
 
+woodchopper_t1_ores = {
+	"default:stone_with_coal"
+}
+
+woodchopper_t2_ores = {
+	"default:stone_with_coal",
+	"default:stone_with_copper",
+	"default:stone_with_tin",
+	"default:stone_with_iron",
+	"default:stone_with_gold"
+}
+
+woodchopper_t3_ores = {
+	"default:stone_with_coal",
+	"default:stone_with_copper",
+	"default:stone_with_tin",
+	"default:stone_with_iron",
+	"default:stone_with_gold",
+	"default:stone_with_mese",
+	"default:stone_with_diamond"
+}
+
 --[[Configures all the tools that can be used to activate the multi-dig
 behaviour]]--
 woodchopper_digging_tools = {
-	"default:axe_wood",
-	"default:axe_stone",
-	"default:axe_bronze",
-	"default:axe_steel",
-	"default:axe_mese",
-	"default:axe_diamond"
+	axe_wood = woodchopper_wood,
+	axe_stone = woodchopper_wood,
+	axe_bronze = woodchopper_wood,
+	axe_steel = woodchopper_wood,
+	axe_mese = woodchopper_wood,
+	axe_diamond = woodchopper_wood,
+	axe_gold = woodchopper_wood,
+	axe_iron = woodchopper_wood,
+	pick_wood = woodchopper_t1_ores,
+	pick_stone = woodchopper_t2_ores,
+	pick_bronze = woodchopper_t3_ores,
+	pick_steel = woodchopper_t3_ores,
+	pick_mese = woodchopper_t3_ores,
+	pick_diamond = woodchopper_t3_ores	
 }
 
 --[[Determines how much of the durability of the toll will be diminished by
--- main.lua
+++ main.lua
@@ -7,10 +7,22 @@ forest.]]--
 
 --[[Small function to check if a list contains a given element]]--
 local function contains(list, x)
-    for _, v in pairs(list) do
-        if v == x then return true end
+    for k, v in pairs(list) do
+		if string.match(x, k) then return true end
+		local b = k == x
+		if not b then b = v == x end
+		if b then return true end
     end
     return false
+end
+
+local function get_table_of_tool(str)
+	for k, v in pairs(woodchopper_digging_tools) do
+		if string.match(str, k) then
+			return v
+		end
+    end
+	return nil
 end
 
 --[[A variable used to determine the maximum amounts of blocks cut in a single
@@ -19,11 +31,11 @@ local blocks_dug_in_iteration = 0
 
 --[[This function digs all the blocks of a given type that are adjacent to the
 block the player interact with]]--
-local function dig_neighbours_block(pos)
+local function dig_neighbours_block(pos, blocks)
 	if blocks_dug_in_iteration >= woodchopper_maximum_blocks_dug then
 		return
 	end
-	local list_pos = minetest.find_nodes_in_area({x=pos.x+1, y=pos.y+1, z=pos.z+1}, {x=pos.x-1, y=pos.y-1, z=pos.z-1}, woodchopper_diggable_blocks)
+	local list_pos = minetest.find_nodes_in_area({x=pos.x+1, y=pos.y+1, z=pos.z+1}, {x=pos.x-1, y=pos.y-1, z=pos.z-1}, blocks)
 
 	for i=1, #list_pos do
 		if list_pos[i] then
@@ -32,7 +44,7 @@ local function dig_neighbours_block(pos)
 			end
 			minetest.dig_node(list_pos[i])
 			blocks_dug_in_iteration = blocks_dug_in_iteration + 1
-			dig_neighbours_block(list_pos[i])
+			dig_neighbours_block(list_pos[i], blocks)
 		end
 	end
 end
@@ -41,14 +53,18 @@ end
 behaviour can only be used during a configurable action, and while a
 configurable tool from a given set is held by the player]]--
 minetest.register_on_dignode(function(pos, oldnode, digger)
+	minetest.chat_send_all("Dug node: " .. oldnode.name .. " with " .. digger:get_wielded_item():get_name())
 	if(digger:is_player()) then
 		if(digger:get_player_control()[woodchopper_activation_button]) then
-      if contains(woodchopper_digging_tools, digger:get_wielded_item():get_name()) then
-        if contains(woodchopper_diggable_blocks, oldnode.name) then
-  				blocks_dug_in_iteration = 0
-  				dig_neighbours_block(pos)
-  			end
-      end
+			local item = digger:get_wielded_item():get_name()
+			if contains(woodchopper_digging_tools, item) then
+				local tot = get_table_of_tool(item)
+				if contains(get_table_of_tool(item), oldnode.name) then
+					minetest.chat_send_all("bloop")
+					blocks_dug_in_iteration = 0
+					dig_neighbours_block(pos, get_table_of_tool(item))
+				end
+			end
 		end
 	end
 end)
